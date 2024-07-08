f29d79555a438b4e55d44577fafe83720f384da7
The Page Guy <thepageguy@mailfence.com>
2024-07-08 00:07:41 +0000

    Finish Course

--- myApp.js
+++ myApp.js
@@ -1,55 +1,129 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
 
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
-let Person;
+// Define the person schema
+const personSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  age: { type: Number },
+  favoriteFoods: { type: [String] }
+});
 
+// Create the Person model
+const Person = mongoose.model('Person', personSchema);
+
+// CRUD operations
 const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+  const person = new Person({
+    name: 'John Doe',
+    age: 30,
+    favoriteFoods: ['Pizza', 'Sushi']
+  });
+
+  person.save((err, data) => {
+    if (err) return done(err); // Return error if saving failed
+    done(null, data); // Pass back saved data if successful
+  });
 };
+
 
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+  Person.create(arrayOfPeople, (err, data) => {
+    if (err) return done(err); // Return error if creation failed
+    done(null, data); // Pass back created data if successful
+  });
 };
+
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({ name: personName }, (err, data) => {
+    if (err) return done(err); // Return error if query failed
+    done(null, data); // Pass back query results if successful
+  });
 };
+
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({ favoriteFoods: food }, (err, data) => {
+    if (err) return done(err); // Return error if query failed
+    done(null, data); // Pass back query result if successful
+  });
 };
+
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById(personId, (err, data) => {
+    if (err) return done(err); // Return error if query failed
+    done(null, data); // Pass back query result if successful
+  });
 };
+
 
 const findEditThenSave = (personId, done) => {
-  const foodToAdd = "hamburger";
+  // Find the person by _id
+  Person.findById(personId, (err, person) => {
+    if (err) return done(err); // Return error if query failed
 
-  done(null /*, data*/);
+    // Modify the person's favoriteFoods
+    person.favoriteFoods.push("hamburger");
+
+    // Save the updated person document
+    person.save((err, updatedPerson) => {
+      if (err) return done(err); // Return error if save failed
+      done(null, updatedPerson); // Pass back updated person if successful
+    });
+  });
 };
+
 
 const findAndUpdate = (personName, done) => {
-  const ageToSet = 20;
-
-  done(null /*, data*/);
+  Person.findOneAndUpdate(
+    { name: personName }, // Search criteria
+    { age: 20 }, // Update age to 20
+    { new: true }, // Options to return the updated document
+    (err, updatedPerson) => {
+      if (err) return done(err); // Return error if update failed
+      done(null, updatedPerson); // Pass back updated person if successful
+    }
+  );
 };
+
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove(personId, (err, removedPerson) => {
+    if (err) return done(err); // Return error if deletion failed
+    done(null, removedPerson); // Pass back removed person if successful
+  });
 };
+
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
 
-  done(null /*, data*/);
+  Person.remove({ name: nameToRemove }, (err, result) => {
+    if (err) return done(err); // Return error if deletion failed
+    done(null, result); // Pass back deletion result if successful
+  });
 };
+
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
 
-  done(null /*, data*/);
+  Person.find({ favoriteFoods: foodToSearch })
+    .sort({ name: 1 })
+    .limit(2)
+    .select('-age')
+    .exec((err, data) => {
+      if (err) return done(err); // Return error if query failed
+      done(null, data); // Pass back query results if successful
+    });
 };
+
 
 /** **Well Done !!**
 /* You completed these challenges, let's go celebrate !
